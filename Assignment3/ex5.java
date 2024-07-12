660f8863102910a0c2114a338c71af8a285a5a99
Taranjeet11kaur <taranjeetkaurbhatova@gmail.com>
2024-07-12 20:23:37 +0530

    First commit

---
+++ Assignment3/ex5.java
@@ -0,0 +1,12 @@
+package Assignment3;
+
+import java.util.Scanner;
+
+public class ex5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter any number: ");
+        int num = sc.nextInt();
+        System.out.println("The number entered by user: " + num);
+    }
+}
