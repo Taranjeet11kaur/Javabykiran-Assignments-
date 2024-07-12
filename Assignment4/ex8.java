63424ffef22bfa4f6147731ca499d9272ce9eec1
Taranjeet11kaur <taranjeetkaurbhatova@gmail.com>
2024-07-12 20:23:41 +0530

    First commit

---
+++ Assignment4/ex8.java
@@ -0,0 +1,13 @@
+package Assignment4;
+
+import java.util.Scanner;
+
+public class ex8 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the side: ");
+        double side = sc.nextDouble();
+        double area = side * side;
+        System.out.println("Area of Square is: " + area);
+    }
+}
---
+++ Assignment4/hw1.java
@@ -0,0 +1,14 @@
+package Assignment4;
+
+import java.util.Scanner;
+
+public class hw1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        int no = sc.nextInt();
+        int cube = (int) Math.pow(no, 3);
+        System.out.println("Cube of number " + no + " is: " + cube);
+
+    }
+}
